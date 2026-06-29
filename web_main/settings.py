7649017891ebd2dbc83b24479e7aaa65f95989f2
as0998 <112304975+as0998@users.noreply.github.com>
2026-06-29 14:44:25 +0900

    Update settings.py

--- web_main/settings.py
+++ web_main/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'win_start',
+     'win_start',
     # 'corsheaders',
     # 'sslserver',
 ]
