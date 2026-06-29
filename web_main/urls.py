dca7ea8602a2199f0ea931f46cdc8ace9c92f894
as0998 <112304975+as0998@users.noreply.github.com>
2026-06-29 14:30:08 +0900

    Update urls.py

--- web_main/urls.py
+++ web_main/urls.py
@@ -20,6 +20,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('win_start/', include("win_start.urls")),
+    # path('win_start/', include("win_start.urls")),
 
 ]
